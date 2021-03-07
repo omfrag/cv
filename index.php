123bb0181964142237e8f3b8459e169c42feca00
omfrag <76168510+omfrag@users.noreply.github.com>
2021-03-07 19:55:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,122 @@
 <html>
     <head>
-        <title></title>
+        <title>SAMA CV</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <div class="content">
-            <div class="part header">
-                <div class="profilePic"></div>
+            <div class="header">
+                <div class="pp"></div>
                 <h1>Ousmane <span>FAYE</span></h1>
-                <h5>Technicien en telecom</h5>
-                <ul class="contact">
-                    <li>Telephone</li>
-                    <li>E-mail</li>
-                    <li>Address</li>
-                </ul>
+                <h3>Technicien en Telecom</h3>
+                <div class="contact">
+                    <h3><i class="fa fa-phone"></i>+221 77 885 16 18</h3>
+                    <h3><i class="fa fa-envelope"></i>omnfy02@gmail.com</h3>
+                    <h3><i class="fa fa-map-marker"></i>hlm grand medine no399</h3>
+                </div>
             </div>
-            <h2>COMPETENCES</h2>
-            <div class="part competence">
+            <!--    FIN  HEADER    -->
+
+            <!--    DEBUT COMPETENCES -->
+            <div class="compet">
+                <h2>COMPETENCES</h2>
                 <ul>
                     <li>Gestion du stress</li>
                     <li>Leadership</li>
                     <li>Travail en equipe</li>
                 </ul>
-                <p>
-                    <strong> Logiciels:</strong> Excel, Word, PowerPoint,<br>
-                    Outlook, Photoshop, Illustrator,<br>
-                    Wordpress
-                </p>
-                <div class="perf">
-                        <div>Allemand</div>
-                        <div class="under"><div class="on Al"></div></div>
-                        <div>Anglais</div>
-                        <div class="under"><div class="on An"></div></div>
-                        <div>Espagnol</div>
-                        <div class="under"><div class="on Es"></div></div>
+                <p><strong>lorem: </strong>Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit.</p>
+                <div class="lang">
+                    <h3>Wolof</h3>
+                    <div class="under"><div class="on wl"></div></div>
+                    <h3>Francais</h3>
+                    <div class="under"><div class="on fr"></div></div>
+                    <h3>Anglais</h3>
+                    <div class="under"><div class="on en"></div></div>
                 </div>
             </div>
-            <h2>FORMATION</h2>
+            <!--   FIN COMPETENCES  -->
+
+            <!--   DEBUT FORMATIONS-->
             <div class="part formation">
+                <h2>FORMATION</h2>
                 <div class="year-one">
-                    <h4>2012 - 2016</h4>
+                    <h3>2012 - 2016</h3>
                     <h5>Dkr - SN</h5>
                 </div>
                 <div class="diplom-one">
-                    <h4>Diplome de Baccalauriat serie S2</h4>
+                    <h3>Diplome de Baccalauriat serie S2</h3>
                     <h5>Groupe Scolaire DIOR</h5>
                 </div>
                 <div class="year-two">
-                    <h4>2012 - 2016</h4>
+                    <h3>2012 - 2016</h3>
                     <h5>Dkr - SN</h5>
                 </div>
                 <div class="diplome-two">
-                    <h4>Diplome de Technicien en telecom et reseau</h4>
+                    <h3>Diplome de Technicien en telecom et reseau</h3>
                     <h5>Ecole Superieur Polytechnique de Dakar</h5>
                 </div>
             </div>
-            <div class="part experiencesProf"></div>
-            <div class="part centerDint"></div>
+            <!--   FIN FORMATIONS-->
+
+            <!--   DEBUT EXPERIENCES  -->
+            <div class="part experiencesProf">
+                <h2>EXPERIENCES PROFESSIONNELLES</h2>
+                <div class="year-one">
+                    <h3>2012 - 2016</h3>
+                    <h5>Dkr - SN</h5>
+                </div>
+                <div class="diplom-one">
+                    <h3>Diplome de Baccalauriat serie S2</h3>
+                    <h5>Groupe Scolaire DIOR</h5>
+                    <ul>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                    </ul>
+                </div>
+                <div class="year-one">
+                    <h3>2012 - 2016</h3>
+                    <h5>Dkr - SN</h5>
+                </div>
+                <div class="diplom-one">
+                    <h3>Diplome de Baccalauriat serie S2</h3>
+                    <h5>Groupe Scolaire DIOR</h5>
+                    <ul>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                    </ul>
+                </div>
+                <div class="year-two">
+                    <h3>2012 - 2016</h3>
+                    <h5>Dkr - SN</h5>
+                </div>
+                <div class="diplome-two">
+                    <h3>Diplome de Technicien en telecom et reseau</h3>
+                    <h5>Ecole Superieur Polytechnique de Dakar</h5>
+                    <ul>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+                    </ul>
+                </div>
+            </div>
+            <!--    FIN EXPERIENCES   -->
+
+            <!--   DEBUT CENTRE D'INTERET  -->
+            <div class="centerDint">
+                <h2>CENTRE D'INTERET</h2>
+                <ul >
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est illum vitae porro dicta?</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est illum vitae porro dicta?</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est illum vitae porro dicta?</li>
+                </ul>
+            </div>
+            <!-- FIN CENTRE D'INTERET-->
         </div>
     </body>
 </html>
